407174be5a5f3fe53f0df96870c111ff3cfc7624
sapirrior <none>
2025-02-19 14:38:23 +0530

    update kick.js

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -7,15 +7,18 @@ module.exports = {
   async execute(message) {
     
     const target = message.mentions.users.first();
+    if(!target) {message.channel.send(`${config.err} **|** Mention someone`); return;};
+    
+    const targetMember = message.guild.members.cache.get(target.id);
+    if(!targetMember) {message.channel.send(`${config.err} **|** That user is not in this server`);return;};
+    
     const user = message.author;
+    if(user.id == target.id) {message.channel.send(`${config.err} **|** You cannot kick yourself`);return;};
+    
     const perm = message.member.permissions.has(PermissionsBitField.Flags.KickMembers);
+    if(!perm) {message.channel.send(`${config.err} **|** You dont have permission to use this command`);return;};
     
-    if(!target) {message.channel.send(`${config.err} **|** Mention someone`); return;}
-    if(!perm) {message.channel.send(`${config.err} **|** You need Kick Members permission`); return;}
-    if(user.id == target.id) {message.channel.send(`${config.err} **|** You cannot kick yourself`); return;}
-    
-    await target.kick().catch(err => {message.channel.send(`${config.err} **|** Unable to kick that user`)});
-    await message.channel.send(`${config.done} **|** Kicked The User\n${config.blank} **|** Username: ${target.username}`);
-    
+    await targetMember.kick().catch(err => {message.channel.send(`${config.err} **|** Unable to kick that user`)});
+    await message.channel.send(`${config.done} **|** Kicked the user\n${config.blank} **|** Username: ${target.username}`);
   },
 };
