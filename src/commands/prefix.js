073f6971f1e3e0ff7d536bba1c0f18d4ff7e7019
sapirrior <h>
2025-02-19 17:12:59 +0530

    update

--- src/commands/prefix.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  name: 'prefix',
-  description: 'Prefix command',
-  async execute(message) {
-    message.channel.send(`**|** The prefix is **${process.env.PREFIX}**`);
-  },
-};
