e9942d70ac400d4756353d31ed4cc2d44b1832e7
Sapirrior <snappyyxd@gmail.com>
2025-02-19 11:27:33 +0530

    Add files via upload

---
+++ src/commands/help.js
@@ -0,0 +1,17 @@
+const modCmds = '`Coming Soon`';
+const utilityCmds = '`help`';
+
+module.exports = {
+  name: 'help',
+  description: 'help command',
+  async execute(message) {
+    
+    let embed = {
+      color: 0x7fbbe7,
+      author: {name: 'Command List'},
+      description: 'Here is the list of all commands\nNeed more help? Come join our guild',
+      fields: [{name: 'Moderation', value: modCmds}, {name: 'Utility', value: utilityCmds}]
+    };
+    message.channel.send({embeds: [embed]});
+  },
+};
