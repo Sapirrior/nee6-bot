19d8913e15df5a363cb8fc813210267fde1425f5
sapirrior <vv>
2025-02-19 17:59:02 +0530

    update

--- nee6.js
+++ nee6.js
@@ -1,54 +1,25 @@
-const { Client, GatewayIntentBits, Events, Collection, Routes, REST } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
+const { Client, GatewayIntentBits, Events, SlashCommandBuilder } = require('discord.js');
+const axios = require('axios')
 require('dotenv').config();
+const client = new Client({intents: GatewayIntentBits.Guilds});
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
-const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
-client.commands = new Collection();
+client.once(Events.ClientReady, () => {
+  console.log('Online!');
+});
 
-// Load Commands Folder
-const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    client.commands.set(command.data.name, command);
-    commands.push(command.data.toJSON());
-}
-
-// Register Commands
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
-
-// Interaction Handler
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
+client.on(Events.InteractionCreate, async i => {
+  if(!i.isChatInputCommand()) return;
+  if(i.commandName !== 'meme') return;
+  
+  const memes = await axios.get('https://meme-api.com/gimme');
+  const meme = memes.data.url;
+  const title = memes.data.title;
+  
+  const embed = {
+    author: {name: title},
+    image: {url: meme}
+  };
+  i.reply({embeds: [embed]});
 });
 
 client.login(process.env.TOKEN)
