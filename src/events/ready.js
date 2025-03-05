93936c82c5c90b262f58b7a112a99e89e80b0f86
sapirrior <snappyyxd@gmail.com>
2025-03-05 21:30:47 +0530

    update

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,10 @@
 const { Events, ActivityType } = require('discord.js');
-const connectDB = require('../../../configs/database');
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
     const guilds = client.guilds.cache.size;
-    connectDB(); // Connect Database
     console.log('Online');
     client.user.setActivity(`with ${guilds} guilds`, {
       type: ActivityType.Streaming,
