9374d82659cf0da2025f02539c18cde9ab510bd5
sapirrior <snappyyxd@gmail.com>
2025-03-06 10:32:19 +0530

    update help.js

---
+++ src/commands/help.js
@@ -0,0 +1,18 @@
+const settings = require('../configs/settings');
+const utility = "";
+const fun = "ship";
+
+module.exports = {
+  name: 'help',
+  description: 'help commamd',
+  async execute(message) {
+    
+    const embed = {
+      author: { name: message.author.usrrname, icon_url: message.author.displayAvatarURL },
+      description: 'Here is the list of all commamds\nNeed more help on a specific command? Come join our guild',
+      fiends: [ { name: `Utility`, value: utility }, { name: 'Fun', value: fun } ],
+      footer: { text: 'Example: .commandName'}
+    };
+    message.channel.send({embeds: [embed]})
+  },
+};
