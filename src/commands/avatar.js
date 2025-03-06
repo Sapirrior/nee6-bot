dcb54d32229e753b6a4def1cbb75d9cd974d4c3f
sapirrior <snappyyxd@gmail.com>
2025-03-06 10:56:19 +0530

    update avatar.js

---
+++ src/commands/avatar.js
@@ -0,0 +1,12 @@
+const settings = require('../configs/settings');
+
+module.exports = {
+  name: 'avatar',
+  description: 'avatar command',
+  async execute(message) {
+    const target = message.mentions.users.first()
+    if(!target) return message.reply(`${settings.err} **|** Please mention someone`);
+    const avatar = target.displayAvatarURL({dynamic: true});
+    const embed = { author: { name: target.username }, image: { url: avatar } }
+  },
+};
