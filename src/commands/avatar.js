43db6ec40dd2d3584ea5a13c535472caa77228d3
sapirrior <snappyyxd@gmail.com>
2025-03-06 10:58:32 +0530

    update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -7,6 +7,7 @@ module.exports = {
     const target = message.mentions.users.first()
     if(!target) return message.reply(`${settings.err} **|** Please mention someone`);
     const avatar = target.displayAvatarURL({dynamic: true});
-    const embed = { author: { name: target.username }, image: { url: avatar } }
+    const embed = { author: { name: target.username }, image: { url: avatar } };
+    message.channel.send({ embeds: [embed] });
   },
 };
