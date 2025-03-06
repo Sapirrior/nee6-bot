31a1093d1b08956e61b65ceea6bcdb7fa9f2a979
sapirrior <snappyyxd@gmail.com>
2025-03-06 10:36:33 +0530

    update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,6 @@
 const settings = require('../configs/settings');
-const utility = "";
-const fun = "ship";
+const utility = "`help ping`";
+const fun = "`ship`";
 
 module.exports = {
   name: 'help',
@@ -10,7 +10,7 @@ module.exports = {
     const embed = {
       author: { name: message.author.usrrname, icon_url: message.author.displayAvatarURL },
       description: 'Here is the list of all commamds\nNeed more help on a specific command? Come join our guild',
-      fiends: [ { name: `Utility`, value: utility }, { name: 'Fun', value: fun } ],
+      fields: [ { name: `Utility`, value: utility }, { name: 'Fun', value: fun } ],
       footer: { text: 'Example: .commandName'}
     };
     message.channel.send({embeds: [embed]})
