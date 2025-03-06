76dd27e45534e26c646f93c0daa530b9b9b27443
sapirrior <snappyyxd@gmail.com>
2025-03-06 13:48:24 +0530

    update

---
+++ src/commands/choose.js
@@ -0,0 +1,23 @@
+const settings = require('../configs/settings');
+const msgs = ['I choose ?!','I like ?!','I love ?!','? is the best choice','definitely ?!','? 100%'];
+
+module.exports = {
+  name: 'choose',
+  description: 'choose commamd',
+  async execute(message, args) {
+    
+    if(args.length == 0) {
+      message.reply(`${settings.err} **|** .choose <item_1>,<item_2>...`);
+      return;
+    };
+    const items = args.join(' ').replace(/[,;]/gi, '|').split('|');
+    if(items.length == 1) {
+      message.reply(`${settings.err} **|** I cannot choose from one item`);
+      return;
+    };
+    
+    let item = items[Math.floor(Math.random() * items.length)];
+		let msg = ', ' + msgs[Math.floor(Math.random() * msgs.length)].replace('?', '**' + item + '**');
+		message.channel.send(`${settings.err} **|** **${message.author.username}**,  ${msg}`);
+  },
+};
