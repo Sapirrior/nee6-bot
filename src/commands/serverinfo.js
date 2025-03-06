8655f7506c931af6d3545e66896d06a946ed39a2
sapirrior <snappyyxd@gmail.com>
2025-03-06 14:15:54 +0530

    update serverinfo.js

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -8,7 +8,7 @@ module.exports = {
     const guild = message.guild
     const owner = await guild.fetchOwner()
     const info = `**Server Name:** ${guild.name}\n**Owner:** ${owner.user.tag}\n**Total Members:** ${guild.memberCount}\n**Created On:** ${guild.createdAt.toUTCString()}\n**Server ID:** ${guild.id}\n**Boost Level:** ${guild.premiumTier || 'None'}\n**Verification Level:** ${guild.verificationLevel}`;
-    const embed = { color: 0x7fbbe7, description: info };
+    const embed = { color: 0x7fbbe7, author: { name: 'Server Info' }, description: info, timestamp: new Date().toISOString() };
     message.channel.send({embeds: [embed]})
   },
 };
