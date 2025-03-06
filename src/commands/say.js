4d08c6642427a5434134930922333af5eb53df19
sapirrior <snappyyxd@gmail.com>
2025-03-06 21:18:07 +0530

    update

---
+++ src/commands/say.js
@@ -0,0 +1,27 @@
+const settings = require('../configs/settings');
+const { PermissionsBitField } = require('discord.js');
+
+module.exports = {
+  name: 'say',
+  description: 'say command',
+  async execute(message, args) {
+    
+    const msg = args.join(' ');
+    if(!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+      message.reply(`${settings.err} **|** You do not have permission to use this command`);
+      return;
+    };
+    
+    if(!args.length) {
+      message.reply(`${settings.err} **|** Please type something`);
+      return;
+    };
+    
+    if(msg.length > 300) {
+      message.reply(`${settings.err} **|** The message is too long`);
+      return;
+    };
+    await message.channel.send(msg).catch(err => { message.reply(settings.errText) });
+    await message.delete().catch(err => { message.reply(settings.errText) });
+  },
+};
