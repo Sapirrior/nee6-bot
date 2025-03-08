8e57ce3a22088b382c8318d17034c10d2438d22a
sapirrior <snappyyxd@gmail.com>
2025-03-08 10:50:43 +0530

    update

--- src/commands/youtube.js
+++ src/commands/youtube.js
@@ -1,22 +1,22 @@
 const settings = require('../configs/settings');
-const { together } = require('../utils/togetherUtil');
 
 module.exports = {
-  name: 'together',
-  description: 'Starts a Discord Together YouTube activity.',
+  name: 'youtube',
+  description: 'youtube activity command',
   async execute(message) {
-
+    
+    const choice = 'youtube';
     const member = message.member;
-    const choice = 'youtube'; // Set the choice to 'youtube'
-
-    const result = await together(member, choice);
-
-    if (result.error) {
-      message.channel.send(result.error);
-    } else if (result.code) {
-      message.channel.send(result.code);
-    } else {
-      message.channel.send(`${settings.err} **|** An unexpected error occurred.`);
-    }
+    const response = await together(member, choice);
+    await message.channel.send(response);
   },
+};
+
+async function together(member, choice) {
+
+  const vc = member.voice.channel?.id;
+  if (!vc) return "You must be in a voice channel to use this command.";
+
+  const invite = await member.client.discordTogether.createTogetherCode(vc, choice);
+  return `${settings.premium} **|** ${invite.code}`;
 };
